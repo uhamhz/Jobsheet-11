d87ba605678a92e0563f05cc9a5c4435a3bc863b
uhamhz <ammarhafizh2404@gmail.com>
2023-11-07 11:10:49 +0700

    fix : excess star

--- SCRIPT/Square.java
+++ SCRIPT/Square.java
@@ -12,7 +12,7 @@ public class Square {
         // Outer Loop
         for (int iOuter = 1; iOuter <= N; iOuter++) {
             // Inner Loop
-            for (int i = 1; i <= N; i++) {
+            for (int i = 1; i <=N; i++) {
                 System.out.print("*");
             }
             System.out.println();
